6f8e0b557ee34e959511a08b6b0925bc035a8b1c
Matthew Herod <matthew.herod@myunidays.com>
2021-02-03 11:11:47 +0000

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,6 @@ import org.jetbrains.kotlin.gradle.dsl.KotlinCommonOptions
 import org.jetbrains.kotlin.gradle.plugin.KotlinCompilation
 import org.jetbrains.kotlin.gradle.plugin.KotlinCompilationToRunnableFiles
 import org.jetbrains.kotlin.gradle.plugin.KotlinTarget
-import org.jetbrains.kotlin.util.capitalizeDecapitalize.toLowerCaseAsciiOnly
 import java.lang.System.getProperty
 import java.lang.System.getenv
 
@@ -106,6 +105,8 @@ kotlin {
     }
 }
 
+val build: Task by tasks
+
 task<Exec>("cleanVcs") {
     workingDir(projectDir)
     inputs.files(fileTree(projectDir))
@@ -121,28 +122,69 @@ task<Exec>("cleanEmptyDirs") {
     commandLine("find", projectSrc, "-type", "d", "-empty", "-delete")
 }
 
-task<Copy>("installBinary") {
-    dependsOn(tasks.getByName("build"))
-    from("$buildDir/bin/native/releaseExecutable/")
+val brewCleanup = task<Exec>("brewCleanup") {
+    onlyIf {
+        // just do this when ran inside idea
+        getProperty("idea.vendor.name") == "JetBrains"
+    }
+    commandLine("brew", "cleanup")
+}
+
+val brewUninstall = task<Exec>("brewUninstall") {
+    onlyIf {
+        file("/usr/local/Cellar/resharkercli").exists()
+                && getProperty("idea.vendor.name") == "JetBrains"
+    }
+    commandLine("brew", "uninstall", "resharkercli")
+    outputs.file("/usr/local/bin/resharkercli")
+    finalizedBy(brewCleanup)
+}
+
+task<Copy>("installDebugBinary") {
+    val linkDebugExecutableNative: Task by tasks
+    dependsOn(brewUninstall, linkDebugExecutableNative)
+    from("$buildDir/bin/native/debugExecutable/")
     include("*.kexe")
     rename { it.substringBefore('.') }
     into("/usr/local/bin/")
 }
 
-task<Copy>("installBrewBinary") {
-    dependsOn(tasks.getByName("build"))
+val installReleaseTask = task<Copy>("installReleaseBinary") {
+    val linkReleaseExecutableNative: Task by tasks
+    dependsOn(brewUninstall, build, linkReleaseExecutableNative)
     from("$buildDir/bin/native/releaseExecutable/")
     include("*.kexe")
     rename { it.substringBefore('.') }
-    into("${getenv("HOMEBREW_FORMULA_PREFIX")}/bin")
+    into(getInstallPath())
+}
+
+task("installBinary") {
+    dependsOn(installReleaseTask)
+}
+
+task<Copy>("installBrewBinary") {
+    dependsOn(installReleaseTask)
 }
 
 task<JavaExec>("run") {
-    dependsOn(tasks.getByName("jvmMainClasses"))
+    val jvmMainClasses: Task by tasks
+    dependsOn(jvmMainClasses)
     main = "resharker.cli.MainKt"
     args("--help")
     val jvm by kotlin.targets.getting
     val main: KotlinCompilation<KotlinCommonOptions> by jvm.compilations
     val runtimeDependencies = (main as KotlinCompilationToRunnableFiles<KotlinCommonOptions>).runtimeDependencyFiles
     classpath = files(main.output.allOutputs, runtimeDependencies)
+}
+
+fun getInstallPath(): String = file(
+    path = getenv("HOMEBREW_FORMULA_PREFIX")
+        .takeUnless { it.isNullOrBlank() }
+        ?.let { path -> file(path).absolutePath }
+        ?.let { path -> "$path/bin/" }
+        ?: "/usr/local/bin/"
+).let { path ->
+    path.exists() || path.mkdirs()
+    println(path.absolutePath)
+    path.absolutePath
 }
